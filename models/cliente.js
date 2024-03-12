7f705b23b225c4e4c57ce7741251e289d4e956ec
shestermonki <faustinoludwin@gmail.com>
2024-03-12 18:10:59 +0100

    modificacion colecion de clientes a usuarios

--- models/cliente.js
+++ models/cliente.js
@@ -4,16 +4,9 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const ClienteSchema = new Schema({
-  nombres: { type: String, required: true },
-  apellidos: { type: String, required: true },
-  pais: { type: String, required: false },
+  nombre: { type: String, required: true },
   email: { type: String, required: true },
-  password: { type: String, required: true },
-  perfil: { type: String, default: "perfil.png", required: true },
   telefono: { type: String, required: false },
-  genero: { type: String, required: false },
-  f_nacimiento: { type: String, required: false },
-  dni: { type: String, required: false },
   createAt: { type: Date, default: Date.now, require: true },
 });
 
