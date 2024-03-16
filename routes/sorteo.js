f1c2881d90e66bcc112c10f4e23d21f075118ba0
shestermonki <faustinoludwin@gmail.com>
2024-03-16 18:35:26 +0100

    nuevas funciones

--- routes/sorteo.js
+++ routes/sorteo.js
@@ -2,10 +2,16 @@ const express = require('express');
 const sorteoController = require('../controllers/sorteoController');
 const hasAdminRole = require('../middlewares/hasAdminRol');
 const api = express.Router();
+const auth = require('../middlewares/authenticate');
+var multiparty = require('connect-multiparty');
+var path = multiparty({ uploadDir: './uploads/sorteos/' })
 // middleware if rol is admin can access
 
 api.get('/sorteos', sorteoController.getAllSorteos);
 api.post('/sorteos', sorteoController.createSorteo);
 api.post('/sorteos/register-user', sorteoController.registerSorteo);
+api.put("/agregar_imgPortada_admin/:id", [auth.auth, path], sorteoController.agregar_imgPortada_admin);
+api.get("/obtener_sorteo_admin/:id", auth.auth, sorteoController.obtener_sorteo_admin);
+api.put("/eliminar_imagen_galeria_admin/:id", auth.auth, sorteoController.eliminar_imagen_galeria_admin);
 
 module.exports = api;
