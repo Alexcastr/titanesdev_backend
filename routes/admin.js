4c5800c6531ac3e666772fd3dfc1d7c310824f46
shestermonki <faustinoludwin@gmail.com>
2024-03-17 20:00:16 +0100

    nuevas funciones registrar admin y editar admin

--- routes/admin.js
+++ routes/admin.js
@@ -6,14 +6,17 @@ const hasAdminRole = require('../middlewares/hasAdminRol');
 
 const api = express.Router();
 
-api.post('/registroAdmin', adminController.registroAdmin);
+api.post('/registroAdmin', auth.auth, adminController.registroAdmin);
 api.post('/loginAdmin', adminController.loginAdmin);
 api.get('/listaAdmin', auth.auth, adminController.listaAdmin);
 api.get('/listarUsuariosAdmin', auth.auth, adminController.listarUsuariosAdmin);
 api.delete('/eliminar_usuario_admin/:id', auth.auth, adminController.eliminar_usuario_admin);
 api.get('/obtener_usuario_admin/:id', auth.auth, adminController.obtener_usuario_admin);
+api.get('/obtener_admin/:id', auth.auth, adminController.obtener_admin);
 //api.get('/obtener_usuario_guest/:id', auth.auth, adminController.obtener_usuario_guest);
 api.put('/actualizar_usuario_admin/:id', auth.auth, adminController.actualizar_usuario_admin);
+api.put('/actualizar_admin/:id', auth.auth, adminController.actualizar_admin);
+
 
 
 module.exports = api;
